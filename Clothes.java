23f65f94fd8ff2446787ef6dbd4231d727552f2a
Banu-Prathap <139442920+Banu-Prathap@users.noreply.github.com>
2023-07-13 14:22:12 +0530

    Add files via upload

---
+++ Clothes.java
@@ -0,0 +1,15 @@
+class Clothes{
+
+	public static void main(String Clothes[]){
+		String brandName= "Jocket";
+		String type = "Syntetic";
+		int price = 199;
+		String size= "M";
+		System.out.println("Main Started");
+		System.out.println("Name of the company is " +brandName);
+		System.out.println("type of cloth " +type);
+		System.out.println("price of the cloth " +price);
+		System.out.println("size of the cloth " +size);
+		System.out.println("Main ended");
+	}
+}
