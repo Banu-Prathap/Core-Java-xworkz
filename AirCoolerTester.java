dd92acf7eaabcd9be50478013ac558fb1f8cfb5a
Banu-Prathap <upbanuprathap@gmail.com>
2023-07-13 13:53:58 +0530

    adding methods related files

---
+++ AirCoolerTester.java
@@ -0,0 +1,32 @@
+class AirCoolerTester{
+	
+	public static void main(String args[]){
+		
+		AirCooler.onOrOff();
+		AirCooler.onOrOff();
+		AirCooler.onOrOff();
+		AirCooler.increasingTemperatur();
+		AirCooler.increasingTemperatur();
+		AirCooler.increasingTemperatur();
+		AirCooler.increasingTemperatur();
+		AirCooler.increasingTemperatur();
+		AirCooler.onOrOff();
+		AirCooler.increasingTemperatur();
+		AirCooler.increasingTemperatur();
+		AirCooler.increasingTemperatur();
+		AirCooler.decreasingTemperature();
+		AirCooler.decreasingTemperature();
+		AirCooler.decreasingTemperature();
+		AirCooler.onOrOff();
+		AirCooler.decreasingTemperature();
+		AirCooler.decreasingTemperature();
+		AirCooler.decreasingTemperature();
+		AirCooler.onOrOff();
+		AirCooler.decreasingTemperature();
+		AirCooler.decreasingTemperature();
+		AirCooler.onOrOff();
+		AirCooler.decreasingTemperature();
+		AirCooler.decreasingTemperature();
+	}
+	
+}
