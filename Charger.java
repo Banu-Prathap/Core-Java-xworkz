a9a7bc1b1d6174621cfb870b4a79bae619c94180
Banu-Prathap <139442920+Banu-Prathap@users.noreply.github.com>
2023-07-13 14:21:21 +0530

    Add files via upload

---
+++ Charger.java
@@ -0,0 +1,16 @@
+class Charger{
+	public static void main(String Charger[]){
+		String brand="MI";
+		String connectivityTechnology= "USB";
+		String connecterType= "Micro USB";
+		String compactableDevices= "MI Devices";
+		String includedComponents= "Charger,MicroUSB cable";
+		System.out.println("Main Started");
+		System.out.println("Brand "+brand);
+		System.out.println("Connectivity Type "+connecterType);
+		System.out.println("Connecter Type "+connecterType);
+		System.out.println("Compactable Devices "+compactableDevices);
+		System.out.println("Included Components "+includedComponents);
+		System.out.println("Main Ended");
+	}
+}
