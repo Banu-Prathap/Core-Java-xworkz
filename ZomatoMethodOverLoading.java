3911d837b16dee743b20d1f7f42e2afd86db55b7
Banu-Prathap <upbanuprathap@gmail.com>
2023-07-14 11:47:45 +0530

    committing arrays files

---
+++ ZomatoMethodOverLoading.java
@@ -0,0 +1,232 @@
+class ZomatoMethodOverLoading{
+	
+	public static double search(String foodName){
+		double priceOfFood=0.0;
+		if( foodName == "Pizza"){
+			// priceOfFood = 99;
+			System.out.println("food name is "+ foodName);
+			return 99;
+			
+		}
+		if( foodName == "Mutton Biryani"){
+			 priceOfFood = 299;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Veg Biryani"){
+			 priceOfFood = 199;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Burger"){
+			 priceOfFood = 149;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Cake"){
+			 priceOfFood = 499;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Ghee Rice"){
+			 priceOfFood = 129;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Jeera Rice"){
+			 priceOfFood = 199;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Roti"){
+			 priceOfFood = 19;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Fish"){
+			 priceOfFood = 299;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Kebab"){
+			 priceOfFood = 70;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Parota"){
+			 priceOfFood = 29;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Veg nuggets"){
+			 priceOfFood = 99;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Chicken Nuggets"){
+			 priceOfFood = 129;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Chichen Biryani"){
+			 priceOfFood = 99;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Chocolate Shake"){
+			 priceOfFood = 140;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Apple Juice"){
+			 priceOfFood = 99;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Chicken 65"){
+			 priceOfFood = 199;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Tomato Rice"){
+			 priceOfFood = 99;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Dal Kichadi"){
+			 priceOfFood = 125;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Idly"){
+			 priceOfFood = 49;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		if( foodName == "Sea Food"){
+			 priceOfFood = 499;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood;
+		}
+		else{
+			System.out.println("Food Iteam not Found " + foodName );
+		}
+		return 0;
+	}
+	
+	public static double search(String foodName, int quantity){
+		double priceOfFood=0.0;
+		if( foodName == "Pizza"){
+			// priceOfFood = 99;
+			System.out.println("food name is "+ foodName);
+			return 99 * quantity;
+			
+		}
+		if( foodName == "Mutton Biryani"){
+			 priceOfFood = 299;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Veg Biryani"){
+			 priceOfFood = 199;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Burger"){
+			 priceOfFood = 149;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Cake"){
+			 priceOfFood = 499;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Ghee Rice"){
+			 priceOfFood = 129;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Jeera Rice"){
+			 priceOfFood = 199;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Roti"){
+			 priceOfFood = 19;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Fish"){
+			 priceOfFood = 299;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Kebab"){
+			 priceOfFood = 70;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Parota"){
+			 priceOfFood = 29;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Veg nuggets"){
+			 priceOfFood = 99;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Chicken Nuggets"){
+			 priceOfFood = 129;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Chichen Biryani"){
+			 priceOfFood = 99;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Chocolate Shake"){
+			 priceOfFood = 140;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Apple Juice"){
+			 priceOfFood = 99;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Chicken 65"){
+			 priceOfFood = 199;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Tomato Rice"){
+			 priceOfFood = 99;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Dal Kichadi"){
+			 priceOfFood = 125;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Idly"){
+			 priceOfFood = 49;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		if( foodName == "Sea Food"){
+			 priceOfFood = 499;
+			System.out.println("food name is "+ foodName);
+			return priceOfFood * quantity;
+		}
+		else{
+			System.out.println("Food Iteam not Found " + foodName );
+		}
+		return 0;
+	}
+	
+	
+}
